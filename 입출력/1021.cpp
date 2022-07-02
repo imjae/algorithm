c198fc5aea89930cdec27cb48135eac8872ecdd9
imjae <code@kakao.com>
2022-07-02 20:36:51 +0900

    1021

---
+++ 입출력/1021.cpp
@@ -0,0 +1,17 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    char data[51];
+    
+    scanf("%s", data);
+
+    printf("%s", data);
+
+    return 0;
+}

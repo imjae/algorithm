d01b9fff657af58ff4f5f83f7a7e780ee1e8c687
imjae <code@kakao.com>
2022-07-03 21:44:16 +0900

    1030

---
+++ 입출력/1030.cpp
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    long long int a;
+
+    scanf("%lld", &a);
+    printf("%lld", a);
+
+    return 0;
+}

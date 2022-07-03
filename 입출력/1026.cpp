9a22c178ae69e577e07dadeddfb5eddd408d83e9
imjae <code@kakao.com>
2022-07-03 21:33:44 +0900

    1026

---
+++ 입출력/1026.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int h, m, s;
+
+    scanf("%d:%d:%d", &h, &m, &s);
+
+    printf("%d", m);
+
+    return 0;
+}

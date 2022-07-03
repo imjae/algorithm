44b3e6c5704ba3383d4206d6e1da3346d00993b2
imjae <code@kakao.com>
2022-07-03 21:34:57 +0900

    1027

---
+++ 입출력/1027.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int y, m, d;
+
+    scanf("%d.%d.%d", &y, &m, &d);
+
+    printf("%02d-%02d-%04d", d, m, y);
+
+    return 0;
+}

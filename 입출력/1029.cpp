bc08d4e62c0a977aba8ea4340ff958279efbc757
imjae <code@kakao.com>
2022-07-03 21:42:49 +0900

    1029

---
+++ 입출력/1029.cpp
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    double d;
+
+    scanf("%lf", &d);
+    printf("%.11lf", d);
+
+    return 0;
+}

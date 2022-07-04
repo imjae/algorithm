c2f4f83b6169da4d306a0caa738a8b03cfc5fb37
임재경 <jakeim11@zempot.com>
2022-07-04 15:32:22 +0900

    1035

---
+++ 입출력/1035.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cin >> hex >> a;
+
+    cout << oct << a;
+
+    return 0;
+}

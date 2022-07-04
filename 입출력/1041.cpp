6f587db30f4d0008bcae29c16a52f998daf5579c
임재경 <jakeim11@zempot.com>
2022-07-04 15:41:33 +0900

    1041

---
+++ 입출력/1041.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    char a;
+
+    cin >> a;
+
+    cout << (char)((int)a + 1);
+
+    return 0;
+}

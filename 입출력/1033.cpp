52eae37c9da20de68b803ffea8d296fe76cf5b4b
임재경 <jakeim11@zempot.com>
2022-07-04 15:24:02 +0900

    1033

---
+++ 입출력/1033.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cin >> a;
+
+    cout << hex << uppercase << a;
+
+    return 0;
+}

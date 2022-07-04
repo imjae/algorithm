40388bafd3c77a1ded27bc5edb45c42ef6e540b3
임재경 <jakeim11@zempot.com>
2022-07-04 15:38:51 +0900

    1040

---
+++ 입출력/1040.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    long long int a;
+
+    cin >> a;
+
+    cout << a * -1;
+
+    return 0;
+}

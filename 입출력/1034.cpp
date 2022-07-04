f77b2541c0128d031c97fe3df1d2ee830ebe7350
임재경 <jakeim11@zempot.com>
2022-07-04 15:27:42 +0900

    1034

---
+++ 입출력/1034.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cin >> oct >> a;
+
+    cout << dec << a;
+
+    return 0;
+}

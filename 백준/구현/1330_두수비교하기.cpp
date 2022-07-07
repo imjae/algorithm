03897dfcca5cef7cd99114b5074f7e3aebd8c3b7
임재경 <jakeim11@zempot.com>
2022-07-07 17:57:13 +0900

    1330, 2741 구현

---
+++ 백준/구현/1330_두수비교하기.cpp
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    if(a > b)
+    {
+        cout << '>';
+    }
+    else if(a < b)
+    {
+        cout << '<';
+    }
+    else
+    {
+        cout << "==";
+    }
+
+    return 0;
+}

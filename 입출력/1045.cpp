fcd3034a50a95c121f9c93263ba444b52457edb4
imjae <code@kakao.com>
2022-07-14 05:31:10 +0900

    포맷 추가해보기 1045

--- 입출력/1045.cpp
+++ 입출력/1045.cpp
@@ -10,7 +10,9 @@ int main()
 
     cin >> a >> b;
 
-    cout << a + b << endl;
+    cout.setf(ios::fixed);
+    cout.precision(6);
+    cout << a + (float)b << endl;
     cout << a - b << endl;
     cout << a * b << endl;
     cout << a / b << endl;

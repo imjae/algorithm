817140680857e541fa68b5b7c25401a00ffbf774
imjae <code@kakao.com>
2022-07-25 21:30:36 +0900

    구현 A+B-2

---
+++ 백준/구현/2558_A+B-2.cpp
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    
+    cin >> a >> b;
+
+    cout << a+b;
+
+    return 0;
+}

3b0601caee9f70d95f66d64cff9746392fcbc5fa
imjae <code@kakao.com>
2022-07-26 20:34:55 +0900

    구현 별찍기3

---
+++ 백준/구현/2440_별찍기3.cpp
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n - i; j >= 1; j--)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}

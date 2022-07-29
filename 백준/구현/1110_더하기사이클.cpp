63f83f123c6e4508a55fd5a97f594f3caa52d759
임재경 <jakeim11@zempot.com>
2022-07-29 16:22:39 +0900

    예전에 하던거네

---
+++ 백준/구현/1110_더하기사이클.cpp
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int a;
+
+    int first = a / 10;
+    int second = a % 10;
+    
+    int tmp = first + second;
+
+    
+
+
+    return 0;
+}
---
+++ 백준/그리디/1541_잃어버린괄화.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+
+    for(int i=0; i<str.length(); i++)
+    {
+        
+    }
+
+    return 0;
+}

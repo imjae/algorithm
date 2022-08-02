22206dd5ea76f3bc8d819ba5e4ec158b87269730
imjae <code@kakao.com>
2022-08-02 21:01:56 +0900

    그리디 1541 잃어버린괄호

--- 백준/그리디/1541_잃어버린괄호.cpp
+++ 백준/그리디/1541_잃어버린괄호.cpp
@@ -1,41 +1,40 @@
-#include<iostream>
-#include<algorithm>
-#include<string.h>
+#include <iostream>
+#include <algorithm>
+#include <string.h>
 using namespace std;
 
 int len, tmp;
 int sum = 0;
-bool flag; // '-'가 등장했는지 체크
+bool flag;
 char str[51];
 
 int main()
 {
 	cin >> str;
-	len = strlen(str); //식의 길이
+	len = strlen(str);
 
 	for (int i = 0; i <= len; i++)
 	{
-
-		if (str[i] == '+' || str[i] == '-' || i == len) // '+','-'가 등장했거나 혹은 수식의 끝일때
+		if (str[i] == '+' || str[i] == '-' || i == len)
 		{
-			if (flag == false) //'-'등장 이전 모든 값 양수화
+			if (flag == false)
 			{
 				sum += tmp;
 				tmp = 0;
 			}
-			else // (flag == true), '-'등장 이후로 모든 값 음수화
+			else
 			{
 				sum -= tmp;
 				tmp = 0;
 			}
 
-			if (str[i] == '-') flag = true; // '-'등장 시 체크
-				
+			if (str[i] == '-')
+				flag = true;
 		}
-		else // tmp변수에 값 누적
+		else
 		{
 			tmp *= 10;
-			tmp += str[i] - '0'; // char 데이터를 정수로 변환
+			tmp += str[i] - '0';
 		}
 	}
 

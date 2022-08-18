c45dca1c68d57f4da079d36ddb68ebf32b85a557
imjae <code@kakao.com>
2022-08-18 20:32:44 +0900

    2743 구현 단어길이재기

---
+++ 백준/구현/2743_단어길이재기.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    getline(cin, str);
+
+    cout << str.size();
+}
